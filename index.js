08f3ae623fd490c2a811ed40fe4d967f2e017cac
Aidan McBride <aidanmcbride@outlook.com>
2021-07-19 14:26:08 -0600

    Set up hello world dev/debug

---
+++ index.js
@@ -0,0 +1,7 @@
+// TESTING, for now
+
+const { makeUser } = require('./user');
+
+const newUserData = {};
+
+console.log('new user:', makeUser(newUserData));
---
+++ user.js
@@ -0,0 +1,13 @@
+// id
+// username
+// password
+// dateCreated
+// dateModified
+
+function makeUser({ id, username, password, dateCreated, dateModified } = {}) {
+  return 'hello world';
+}
+
+module.exports = {
+  makeUser,
+};
